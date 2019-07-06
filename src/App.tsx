f2926940759674129788ea12efd9a2ebb33ed7cd
buko106 <buko1062000@yahoo.co.jp>
2019-07-06 23:17:18 +0900

    add auth

--- src/App.tsx
+++ src/App.tsx
@@ -2,33 +2,69 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        and
-        <a
-          className="App-link"
-          href="https://aws-amplify.github.io"
-          target="_blank"
-        >
-          AWS Amplify
-        </a>
-      </header>
-    </div>
-  );
+import Amplify, { Auth } from 'aws-amplify';
+import { CognitoUser } from '@aws-amplify/auth';
+import awsconfig from './aws-exports';
+import { withAuthenticator } from 'aws-amplify-react';
+
+Amplify.configure(awsconfig);
+
+interface Props {
 }
 
-export default App;
+interface State {
+  username?: string;
+}
+
+class App extends React.Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = {};
+  }
+
+  componentWillMount(): void {
+    Auth.currentAuthenticatedUser().then((user: CognitoUser) => {
+      this.setState({
+        username: user.getUsername(),
+      });
+    })
+  }
+
+  render() {
+    const {username} = this.state;
+    return (
+      <div>
+        Hello {username}.
+      </div>
+    )
+  }
+}
+
+const usernameAttributes = 'User ID';
+
+const signUpConfig = {
+  hideAllDefaults: true,
+  defaultCountryCode: '1',
+  signUpFields: [
+    {
+      label: usernameAttributes,
+      key: 'username',
+      required: true,
+      displayOrder: 1,
+      type: 'string'
+    },
+    {
+      label: 'Password',
+      key: 'password',
+      required: true,
+      displayOrder: 2,
+      type: 'password'
+    },
+  ]
+};
+
+export default withAuthenticator(App, {
+  signUpConfig,
+  usernameAttributes
+});
+
